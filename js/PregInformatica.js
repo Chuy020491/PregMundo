cc57dc8241d22abfe1a062943dfcc5b8587f1691
Chuy020491 <jegoel02@gmail.com>
2014-07-24 23:16:22 -0500

    V 0.3
    Menu de PregMundo

---
+++ js/PregInformatica.js
@@ -0,0 +1,17 @@
+ $(function(){
+            
+    $("#boton").on('click',function(){
+      var res=$("#respuesta").val().toLowerCase();
+             
+   
+    if ( res =="steven job")
+    
+        $("#resultado").html("correcto");
+        else
+          $("#resultado").html("No es correcto");   
+    
+    }); 
+    
+    });
+    
+   
--- js/siguiente.js
+++ js/siguiente.js
@@ -1,8 +1,56 @@
-/*$(function(){
-var sig=$("#Log");
-    sig.on('dblclick',function(){
-    sig.attr('src','Log.html');
     
-    });
+function validarTextos(){
+    var band = true;
+    var boton=document.getElementById("btn");
+    var nombre= document.getElementById("nom").value;
+     var elem=document.getElementById("nom");   
+        
+        if(nombre == ""){
+        elem.style.backgroundColor="#76ff00";
+        elem.focus();
+            band = false;
+        }else{
+    elem.style.backgroundColor="";
+            
+      }
 
-});*/
+var apellido=document.getElementById("ape").value;
+var elemt=document.getElementById("ape");
+    if(apellido == ""){
+    elemt.style.backgroundColor="#76ff00";
+    band = false;
+    }else{
+    elemt.style.backgroundColor="";
+    }
+    
+var Edad= document.getElementById("ed").value;
+     var elem=document.getElementById("ed");  
+    
+    if((Edad == "")||(isNaN(Edad))){
+        elem.style.backgroundColor="#76ff00";
+        band = false;
+        }else{
+    elem.style.backgroundColor="";    
+        
+        }        
+var elem1=document.getElementById("sex1");
+    var elem2=document.getElementById("sex2");
+    var mensaje=document.getElementById("msj");
+    elem1.checked 
+    
+    if(!(elem1.checked)&& !(elem2.checked)){
+    mensaje.innerHTML="Debes elegir tu sexo";
+        mensaje.style.display="block";
+        mensaje.style.color="red";
+    band = false;
+    }else{
+    mensaje.style.display="none";
+    }
+    
+    
+if(band){
+boton.style.display="block";
+}
+
+}
+    
